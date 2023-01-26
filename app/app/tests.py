87383600e10803dadfe6870c440029284a0b9d81
Akpomudje Victor <akpomudje@gmail.com>
2023-01-25 23:53:11 -0600

    added postgres

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,15 +5,12 @@ Sample tests for the app.
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         """Test that two numbers are added together"""
         self.assertEqual(calc.add(3, 8), 11)
-    
+
     def test_substract_numbers(self):
         """Test that values are substracted and returned"""
         self.assertEqual(calc.substract(11, 5), 6)
-
-
-
-   
